0bb31b8679315d587d3095944cbf5dc81d4872a0
VitalikGrinya <vitalik240899@ya.ru>
2024-04-08 21:34:28 +0300

    Update Method TakeDamage

--- Assets/Scripts/HealthValueChanger.cs
+++ Assets/Scripts/HealthValueChanger.cs
@@ -7,42 +7,28 @@ using System;
 public class HealthValueChanger : MonoBehaviour
 {
     [SerializeField] private Health _health;
+    [SerializeField] private float _damage;
+    [SerializeField] private float _heal;
 
     public event Action Change;
 
-    private float _damage;
-    private float _heal;
-
     private float CurrentHealth => _health.CurrentHealth;
     private float MaxHealth => _health.MaxHealth;
-
 
     private void Awake()
     {
         _health = GetComponent<Health>();
     }
 
-    public void ClickDamageButton()
+    public void TakeDamage()
     {
-        TakeDamage(CurrentHealth);
+        _health.SetCurrentHealth(Mathf.Clamp(CurrentHealth - _damage, 0, MaxHealth));
         Change?.Invoke();
     }
 
-    public void ClickHealButton()
+    public void TakeHeal()
     {
-        TakeHeal(CurrentHealth);
+        _health.SetCurrentHealth(Mathf.Clamp(CurrentHealth + _heal, 0, MaxHealth));
         Change?.Invoke();
-    }
-
-    private void TakeDamage(float health)
-    {
-        _damage = 10;
-        _health.SetCurrentHealth(Mathf.Clamp(health - _damage, 0, MaxHealth));
-    }
-
-    private void TakeHeal(float health)
-    {
-        _heal = 10;
-        _health.SetCurrentHealth(Mathf.Clamp(health + _heal, 0, MaxHealth));
     }
 }
